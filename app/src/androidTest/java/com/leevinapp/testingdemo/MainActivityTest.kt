b136f7c56067285821cedf8e308239dbb4e042c1
Leevin Yang <leevin163@163.com>
2021-08-08 14:26:09 +0800

    Add UI testing with mockwebserver

---
+++ app/src/androidTest/java/com/leevinapp/testingdemo/MainActivityTest.kt
@@ -0,0 +1,104 @@
+package com.leevinapp.testingdemo
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    private val mockWebServer = MockWebServer()
+
+    @Before
+    fun setup() {
+        mockWebServer.start(8080)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun testSuccessfulResponse() {
+        mockWebServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse()
+                        .setResponseCode(200)
+                        .setBody(loadLocalResponse("success_response.json"))
+            }
+        }
+        activityRule.launchActivity(null)
+
+        Espresso.onView(withId(R.id.progress_bar))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        Espresso.onView(withId(R.id.textviewSuccess))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        Espresso.onView(withId(R.id.textview))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+    }
+
+    @Test
+    fun testFailedResponse() {
+        mockWebServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse().throttleBody(1024, 5, TimeUnit.SECONDS)
+            }
+        }
+
+        activityRule.launchActivity(null)
+
+        Espresso.onView(withId(R.id.progress_bar))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        Espresso.onView(withId(R.id.textviewSuccess))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        Espresso.onView(withId(R.id.textview))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        Espresso.onView(withId(R.id.textview))
+                .check(ViewAssertions.matches(withText(R.string.error_message)))
+    }
+
+    @Test
+    fun testEmptyResponse() {
+        mockWebServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse()
+                    .setResponseCode(200)
+                    .setBody("[]")
+            }
+        }
+
+        activityRule.launchActivity(null)
+
+        Espresso.onView(withId(R.id.progress_bar))
+            .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        Espresso.onView(withId(R.id.textviewSuccess))
+            .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
+        Espresso.onView(withId(R.id.textview))
+            .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        Espresso.onView(withId(R.id.textview))
+            .check(ViewAssertions.matches(withText(R.string.no_data_message)))
+    }
+
+    fun loadLocalResponse(path:String):String {
+        return ResourceFile(path).readText()
+    }
+
+}
---
+++ app/src/androidTest/java/com/leevinapp/testingdemo/ResourceFile.kt
@@ -0,0 +1,26 @@
+package com.leevinapp.testingdemo
+
+import java.nio.charset.Charset
+
+class ResourceFile(
+    private val filePath: String
+) {
+    private val loader: ClassLoader
+        get() = Thread.currentThread().contextClassLoader ?: error("Class loader not available!")
+
+
+    fun exist(): Boolean {
+        return loader.getResource(filePath) != null
+    }
+
+    fun readText(charset: Charset = Charsets.UTF_8): String {
+        if (!exist()) {
+            error("File $filePath does not exist or cannot be accessed")
+        }
+
+        return loader
+            .getResourceAsStream(filePath)
+            .bufferedReader(charset)
+            .use { it.readText() }
+    }
+}
