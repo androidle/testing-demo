42bc71054e30a6c7557951d8aa122e8387b171f4
Leevin Yang <leevin163@163.com>
2021-11-21 12:27:36 +0800

    Add repository layer testing

---
+++ app/src/test/java/com/leevinapp/testingdemo/repository/UserDataRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.leevinapp.testingdemo.repository
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UserDataRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getUserData() {
+    }
+}
